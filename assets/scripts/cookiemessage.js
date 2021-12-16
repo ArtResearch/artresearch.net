f3d17a9eafbcda3d99669749dd82981a948ac3b8
Sophia <sophiasideris@gmail.com>
2021-12-16 15:01:54 +0200

    Cookies Final

--- assets/scripts/cookiemessage.js
+++ assets/scripts/cookiemessage.js
@@ -17,17 +17,77 @@ class CookieMsg extends HTMLElement {
     this._shadowRoot = this.attachShadow({ mode: "open" });
     this._shadowRoot.innerHTML = `
     <div class ="CookieMessageContainer">
-       <h4>Cookie Warning</h4>
+       <h4 style="font-size: 24px !important;
+                  margin-top: 10px !important;
+                  margin-bottom: 10px !important;">Cookie Warning</h4>
             <p>
             This website stores data such as cookies to enable site functionality including analytics and personalization. By using this website, you automatically accept that we use cookies. 
             </p>
             <div class="CookieButtons">
-                <button type="button" class="AcceptButton" id="btnAccept">
+                <button type="button" class="AcceptButton button-75 " id="btnAccept">
                     Got it!
                 </button>
                 
             </div>
     </div>
+    <style>
+    .button-75 {
+  box-shadow: rgb(0 0 0 / 15%) 0px 0px 15px;
+  align-items: center;
+  background-image: linear-gradient(135deg, #083e59 45%, #528daf);
+  border: 0;
+  border-radius: 10px;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  font-family: "Codec cold",sans-serif;
+  font-size: 16px;
+  font-weight: 700;
+  height: 54px;
+  justify-content: center;
+  letter-spacing: .4px;
+  line-height: 1;
+  max-width: 100%;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 3px;
+  text-decoration: none;
+  text-transform: uppercase;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.button-75:active {
+  outline: 0;
+}
+
+.button-75:hover {
+  outline: 0;
+}
+
+.button-75 span {
+  transition: all 200ms;
+}
+
+.button-75:hover span {
+  transform: scale(.9);
+  opacity: .75;
+}
+
+@media screen and (max-width: 991px) {
+  .button-75 {
+    font-size: 15px;
+    height: 50px;
+  }
+
+  .button-75 span {
+    line-height: 50px;
+  }
+}
+    </style>
     `;
   const Container = this._shadowRoot.querySelectorAll(".CookieMessageContainer");
    const btnACC = this._shadowRoot.querySelectorAll(".AcceptButton");
