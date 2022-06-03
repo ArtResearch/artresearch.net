7f4ff983df97d00934ed362ef1f353de6d256614
mafragias <mafragias@hotmail.com>
2022-06-03 17:08:03 +0300

    t

--- assets/scripts/req.js
+++ assets/scripts/req.js
@@ -8,13 +8,23 @@ class ReqSolr extends HTMLElement {
   constructor() {
 
     super();
-    // var h = new Header( "Access-Control-Allow-Origin", "https://dev.artresearch.net" );
-    this._shadowRoot = this.attachShadow({ mode: "open" });
-    // const core = this.getAttribute("core");
-    // const keyword = this.getAttribute("keyword");
-    // const artworks = "https://solr.artresearch.net/solr/artists_v5/select?defType=dismax&facet.field=roles_str&facet=true&indent=true&q.op=OR&q=leonardo%20da";
-    $.getJSON('https://solr.artresearch.net/solr/artists_v5/select?defType=dismax&facet.field=roles_str&facet=true&indent=true&q.op=OR&q=leonardo%20da', function(data) {
-      // JSON result in `data` variable
+    $.ajax({
+        url: 'https://solr.artresearch.net/solr/artists_v5/select',
+        dataType: 'json',
+        type: 'POST',
+        crossDomain: true,
+        beforeSend: function(request) {
+          request.setRequestHeader("Access-Control-Allow-Origin", "*");
+        },
+        success: function(data) {
+
+            $("#results").append('all good');
+            alert(data);
+        },
+          error: function() {
+            $("#results").append("error");
+            alert('error');
+        }
     });
   }
 }
