1e78fbed6bb8168065349337410a20ed3a769795
Sophia <sophiasideris@gmail.com>
2022-06-03 14:34:03 +0300

    test

--- assets/scripts/require.js
+++ assets/scripts/require.js
@@ -1,7 +1,3 @@
-/** vim: et:ts=4:sw=4:sts=4
- * @license RequireJS 2.3.5 Copyright jQuery Foundation and other contributors.
- * Released under MIT license, https://github.com/requirejs/requirejs/blob/master/LICENSE
- */
 var requirejs, require, define;
 !(function (global, setTimeout) {
   function commentReplace(e, t) {
